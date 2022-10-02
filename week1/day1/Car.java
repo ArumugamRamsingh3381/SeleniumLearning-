f266aa0ca1faaca72da9080455e14fe2c0004d88
ArumugamRamsingh3381 <114843438+ArumugamRamsingh3381@users.noreply.github.com>
2022-10-02 08:53:22 +0530

    Add files via upload
    Week1

---
+++ week1/day1/Car.java
@@ -0,0 +1,10 @@
+package week1.day1;
+
+public class Car {	
+	int myCarnumber=3381;
+	char makeModel= 'H';
+	boolean isAirbagavailable= true;
+	String myCarModel = "Honda";
+	long engineCubiccapacity= 1200;
+		
+}
---
+++ week1/day1/Factorial.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+public class Factorial {
+public static void main(String arg[])
+	
+	{
+	
+	 int n = 5; 
+	 int factorial = 1;
+	    for(int i = n; i >= 2; i--){
+	      factorial = factorial * i;
+	    }
+	    System.out.println(factorial);
+	  }
+
+    }
+
